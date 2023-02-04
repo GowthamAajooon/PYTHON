1840a3741125813780397624b9e1c2069c4a28da
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-04 15:53:31 +0530

    Update occurences_string.py

--- LIST/OCCURENCES/occurences_string.py
+++ LIST/OCCURENCES/occurences_string.py
@@ -1,3 +1,26 @@
 a ="aabbbaabba"
 h = [idx for idx,val in enumerate(a) if 'a' == val]
 print(*h)
+
+
+'''
+from collections import Counter
+n = input()
+s =''
+for k,v in Counter(n).items():
+    s+=str(k)+str(v)
+print(s)
+    
+'''
+n = input()
+m = list(set(n))
+m.sort()
+k = []
+for i in range(len(m)):
+    k.append(n.count(m[i]))
+s =''
+for i in range(len(m)):
+    s+=m[i]
+    s+=str(k[i])
+print(s)
+
