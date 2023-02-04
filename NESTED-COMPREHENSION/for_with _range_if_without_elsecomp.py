da8bb27751c73f7088bcabd0a517966806c39b12
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-04 09:12:05 +0530

    Update for_with _range_if_without_elsecomp.py

--- NESTED-COMPREHENSION/for_with _range_if_without_elsecomp.py
+++ NESTED-COMPREHENSION/for_with _range_if_without_elsecomp.py
@@ -3,3 +3,8 @@ print(*[i for i in range(1,int(input())+1) if i%5!=0 and i%7!=0 ])
 
 n = int(input())
 print(*[i for i in range(n//2,((n//2)+1)*-1,-1) if i!=0])
+
+
+n = int(input())
+l = list(map(int,input().split()))
+print(sum([i for i in l if i>0]))
