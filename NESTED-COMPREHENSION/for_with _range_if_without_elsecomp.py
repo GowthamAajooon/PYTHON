aa755801674a571a128bb22fae5697a44cc19c2b
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-04 11:58:31 +0530

    Update for_with _range_if_without_elsecomp.py

--- NESTED-COMPREHENSION/for_with _range_if_without_elsecomp.py
+++ NESTED-COMPREHENSION/for_with _range_if_without_elsecomp.py
@@ -15,23 +15,3 @@ l = list(map(int,input().split()))
 print(sum([i for i in l if i%2!=0]))
 
 
-n1,n2 = int(input()),int(input())
-m1,m2=n1,n2
-if n1>0 and n2>0:
-    if n1==n2:
-        m = [[int(input()) for i in range(n1)] for j in range(n2)]
-        print("Primary diagonal elements:")
-        for i in range(n1):
-            for j in range(n2):
-                if i == j:
-                    print(m[i][j])
-        print("Secondary diagonal elements:")  
-        for i in range(m1):
-            for j in range(m2):
-                if i+j == m1-1:
-                    print(m[i][j])
-    else:
-        print("Row and column size should be same")
-else:
-    print("Row and column size should not be negative")
-    
