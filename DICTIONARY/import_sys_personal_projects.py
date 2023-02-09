080c4d99b5f8435f3154eb37a76257dbb18e206a
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-09 22:14:19 +0530

    Update import_sys_personal_projects.py

--- DICTIONARY/import_sys_personal_projects.py
+++ DICTIONARY/import_sys_personal_projects.py
@@ -27,3 +27,50 @@ if "0" in b:
     print("]")
 else:
     print("Error. No root found")
+
+    '''
+folders  = [
+{id: 27, parentId: 15, name: 'projects'},
+{id: 81, parentId: 27, name: 'novel'},
+{id: 15, parentId: 0, name: 'personal'},
+{id: 35, parentId: 27, name: 'blog'},
+]
+'''
+'''
+import sys
+msg=sys.stdin.readlines()
+print(msg)
+
+'''
+
+m = dict()
+h = []
+
+while m!="]":
+    m = input()
+    h.append(m)
+    
+k=[]
+for i in range(1,len(h)-1):
+    k.append(h[i].replace("{","").replace("}","").replace(",","").replace("id: ","").replace("parentId: ","").replace("name: ","").replace("'","").split())
+a,b,c=[],[],[]
+for i in k:
+    a.append(i[0])
+    b.append(i[1])
+    c.append(i[2])
+id1,pid = {},{}
+for i in range(len(k)):
+    id1[a[i]],pid[a[i]]  = b[i],c[i]
+
+
+print("label = [")
+output=""
+for k,v in pid.items():
+    print("    '",end="")
+    output=v
+    while(id1[k]!="0"):
+        parentval=pid[id1[k]]
+        output=parentval+"/"+output
+        k=id1[k]
+    print(output,end="'\n")
+print("]")
