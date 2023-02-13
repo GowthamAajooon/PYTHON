01754c955a877ceddd5ceb0faf5d488dd180b83a
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-13 22:59:15 +0530

    Create common_longest_subsequence.py

---
+++ COMBINATION/common_longest_subsequence.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+s1 = "ABCDGHE"
+s2 = "ACDGHRE"
+# CDGH
+a1 = []
+for i in range(len(s1)):
+    a = combinations(s1,i)
+    for i in a:
+        a1.append(i)
+a2 = []
+for i in range(len(s2)):
+    a = combinations(s2,i)
+    for i in a:
+        a2.append(i)
+ac = list(set(a1+a2))
+for i in ac:
+    if "CDGH" == "".join(i):
+        print("Yes")
+        break
+else:
+    print("No")
