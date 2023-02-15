4d57f03d31a159619551486225f8f08497d55622
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2023-02-15 11:56:32 +0530

    Create constructor_positional_args.py

---
+++ OOPS/CONSTRUCTOR/constructor_positional_args.py
@@ -0,0 +1,27 @@
+class Student:
+    
+    def __init__(self,*args):
+        if len(args)==0:
+            self.name="Ram"
+            self.age=23
+        elif len(args)==1:
+            self.name = args[0]
+        elif len(args)==2:
+            self.name = args[0]
+            self.age = args[1]
+        elif len(args)==3:
+            self.name=args[0]
+            self.age = args[1]
+            self.marks = args[2]
+
+s0 = Student()
+print(s0.name,s0.age)
+
+s1 = Student("velu")
+print(s1.name)
+
+s2 = Student("sham",23)
+print(s2.name,s2.age)
+
+s3 = Students("Deva",18,98)
+print
